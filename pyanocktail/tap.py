1e4dd8e731b6b34bf9e8cfe38ee06381f3c455fe
bverdu <bertrand.verdu@gmail.com>
2012-10-09 22:07:49 +0200

    Cleanup + twisted plugin + Setup script + Archlinux package

---
+++ pyanocktail/tap.py
@@ -0,0 +1,30 @@
+# ==== twisted/plugins/pyanocktail_plugin.py ====
+# python modules
+import os
+
+# - Twisted modules -
+from twisted.python import usage
+
+# - pyanocktail modules -
+from pyanocktail.config import defaultConf
+from pyanocktail import pyanocktaild
+
+class Options(usage.Options):
+    try:
+        dirname = os.path.expanduser("~/.pianocktail")
+    except:
+        dirname = "/etc/pianocktail"
+    conf = defaultConf(dirname)
+    config = dirname
+    basedir = conf.installdir
+    port = conf.httpport
+    optParameters = [
+    ['config', 'c', config, 'Configuration file path'],
+    ['basedir', 'd', basedir, 'Base directory for HTML templates'],
+    ['port', 'p', port, 'http port for the web console']
+    ]
+        
+    
+    
+def makeService(config):
+        return pyanocktaild.makeService(config)
