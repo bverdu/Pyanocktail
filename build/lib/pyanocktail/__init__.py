2c6c41ac98e0e7d27fbad903cbb4c9e6298c24a9
bverdu <bertrand.verdu@gmail.com>
2013-11-03 21:38:30 +0100

    Deleted old buils

--- build/lib/pyanocktail/__init__.py
+++
@@ -1,3 +0,0 @@
-'''
-pyanocktaild multiservice
-'''
--- build/lib/twisted/plugins/pyanocktail2_plugin.py
+++
@@ -1,7 +0,0 @@
-from twisted.application.service import ServiceMaker
-
-pianocktail2 = ServiceMaker(
-                           "Pianocktail", 
-                           "pyanocktail.tap",
-                           "A Pianocktail service",
-                           "pianocktail")
